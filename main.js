e6c5976f50bcb6feb02406900e64c7e552a732b6
Egor <klukinegor2@gmail.com>
2026-04-03 16:00:55 +0300

    test

---
+++ main.js
@@ -0,0 +1,28 @@
+const workerA = { position: 0, range: 2 };
+const workerB = { position: 10, range: 2 };
+
+function paintingOfStreetPoles(arrayA, arrayB) {
+  const startPosA = arrayA.position - arrayA.range;
+  const endPosA = arrayA.position + arrayA.range;
+
+  const startPosB = arrayB.position - arrayB.range;
+  const endPosB = arrayB.position + arrayB.range;
+
+  const startCombiningRange = Math.min(startPosA, startPosB);
+  const endCombiningRange = Math.max(endPosA, endPosB);
+
+  const isIntersection = endPosA >= startPosB && endPosB >= startPosA;
+
+  let count;
+
+  if (isIntersection) count = endCombiningRange - startCombiningRange + 1;
+  else {
+    count = endPosA - startPosA + 1 + endPosB - startPosA + 1;
+  }
+  console.log(
+    `${arrayA.name}: от ${startPosA} до ${endPosA}\n${arrayB.name}: от ${startPosB} до ${endPosB}`,
+  );
+  console.log(`Объединение: [${startCombiningRange}...${endCombiningRange}]`);
+  console.log(`Количество: ${count}`);
+}
+paintingOfStreetPoles(workerA, workerB);
